61e0816fb94e55581c5e7f8f8d9ed899c562de9a
Evghenii Kondratenko <evghenii.kondratenko@merantix.com>
2020-12-19 10:48:42 +0100

    Initial folders and an empty core file
    Includes the template for the output website and the gitignore file.
    No code yet.

---
+++ src/monologue/core.clj
@@ -0,0 +1,6 @@
+(ns monologue.core)
+
+;; Commands
+;; TODO: helper to create a post (current dir or specified thoughts directory)
+;;       always generates a txt file with current date as name
+;; TODO: generate a blog with specified input (thoughts dir) and output (site dir)
