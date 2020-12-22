dbaa1634495babed716d307103a92afa9eb54872
Evghenii Kondratenko <evghenii.kondratenko@merantix.com>
2020-12-22 17:55:48 +0100

    Generating the index page
    Added hiccup and quite naive page creation based on naive templating.
    If I ever see a performance hit (meaning: I'll run the site generator
    get a coffee, drink the damn coffee and the site is still not ready) then
    I can look into: a) algorithms (brains required); b) templating libs
    (no brains required).
    
    For now it works.
    
    Tweaked the formatting and styles a tiny bit as well. Now I dig how it looks.

--- src/monologue/core.clj
+++ src/monologue/core.clj
@@ -1,17 +1,22 @@
 (ns monologue.core
   (:require [clojure.java.io :as io]
             [clojure.string :as string]
-            [clojure.tools.cli :refer [parse-opts]])
+            [clojure.tools.cli :refer [parse-opts]]
+            [hiccup.core :refer [html]])
   (:import [java.time LocalDate]
            [java.time.format DateTimeFormatter])
   (:gen-class))
+
+
+(def date-fmt (DateTimeFormatter/ofPattern "yyyyMMdd"))
+(def output-fmt (DateTimeFormatter/ofPattern "dd MMM, yyyy"))
+
 
 (defn today-date
   "Returns today's date in file-name friendly format.
   The date is local date."
   []
-  (let [fmt (DateTimeFormatter/ofPattern "yyyyMMdd")]
-    (.format (LocalDate/now) fmt)))
+  (.format (LocalDate/now) date-fmt))
 
 (def supported-actions #{"add" "generate"})
 
@@ -48,14 +53,61 @@
       :else                                      {:message (usage summary)})))
 
 (defn add-thought
+  "Creates a txt file with today's date as the filename.
+  If the parent directories don't exist - creates it."
   [{:keys [thoughts]}]
   (let [new-thought-file (str thoughts "/" (today-date) ".txt")]
     (io/make-parents new-thought-file)
     (spit new-thought-file "")
     new-thought-file))
 
+(defn read-template
+  []
+  (line-seq (io/reader (io/resource "template.html"))))
+
+(defn content-file->html-str
+  [content-file]
+  (for [line (line-seq (io/reader content-file))]
+    (str line "<br />")))
+
+(defn generate-index-page
+  "Takes 10 latest posts and creates an HTML page of them."
+  [posts site]
+  (let [latest          (take 10 (reverse posts))
+        index-file-name (str site "/index.html")
+        format-title    (fn [title]
+                          (as-> (string/split title #".txt") t
+                            (first t)
+                            (LocalDate/parse t date-fmt)
+                            (.format t output-fmt)))]
+    (io/make-parents index-file-name)
+    (with-open [index-file (io/writer index-file-name :encoding "UTF-8")]
+      (doseq [line (read-template)]
+        (if (string/includes? line "CONTENT")
+          (.write index-file
+                  (html[:div
+                        (for [[title content-file] latest]
+                          [:div
+                           [:h3 (format-title title)]
+                           [:div (content-file->html-str content-file)]
+                           [:hr]])]))
+          (.write index-file line)))
+      (.flush index-file))))
+
 (defn generate-site
-  [opts])
+  "Generates the website by using the template.html found in the resources
+  and the thoughts found in the thoughts location.
+  Two html pages are created: index.html and archive.html
+  If there's an img directory in the thoughts location, that directory is moved
+  to the site direcoty."
+  [{:keys [thoughts site]}]
+  (let [posts             (->> (file-seq (io/file thoughts))
+                               (filter #(.isFile %))
+                               (map (fn [f] {(.getName f) f}))
+                               (into (sorted-map)))
+        archive-file-name (str site "/archive.html" )]
+    (io/make-parents archive-file-name)
+    (generate-index-page posts site)))
 
 (defn -main [& args]
   (let [{:keys [message action options]} (parse-args args)]
@@ -71,8 +123,11 @@
 
   (parse-args ["-tsomewhere" "-ssomewhereelse"  "add"])
 
-  (parse-args ["generate" "--thoughts=blah" "--site=megalbah"])
+  (parse-args ["generate" "--thoughts=blah" ])
 
+
+  (generate-site {:thoughts "../countgizmo.github.io/thoughts"
+                  :site "../countgizmo.github.io/site"})
 
 
   )
