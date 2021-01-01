93253d9fb221687f2a1a5b623cc6ac11ab396159
countgizmo <count.gizmo@gmail.com>
2021-01-01 10:37:26 +0100

    Add archive page and year pages generation
    Following the same approach as with the index page generation, I'm using
    the same template but populate it with links.
    
    Every year page is the same as an index page but with posts for that year
    only, so I could re-use a lot of code.
    
    Tweaked the styles a bit as well.

--- src/monologue/core.clj
+++ src/monologue/core.clj
@@ -8,7 +8,7 @@
   (:gen-class))
 
 
-(def date-fmt (DateTimeFormatter/ofPattern "yyyyMMdd"))
+(def input-fmt (DateTimeFormatter/ofPattern "yyyyMMdd"))
 (def output-fmt (DateTimeFormatter/ofPattern "dd MMM, yyyy"))
 
 
@@ -16,7 +16,7 @@
   "Returns today's date in file-name friendly format.
   The date is local date."
   []
-  (.format (LocalDate/now) date-fmt))
+  (.format (LocalDate/now) input-fmt))
 
 (def supported-actions #{"add" "generate"})
 
@@ -70,29 +70,54 @@
   (for [line (line-seq (io/reader content-file))]
     (str line "<br />")))
 
+(defn thought-title->date
+  [title]
+  (-> (string/split title #".txt")
+      first
+      (LocalDate/parse input-fmt)))
+
+(defn ->post
+  [[date content-file]]
+  [:div
+   {:class "post"}
+   [:h3 (.format date output-fmt)]
+   [:div (content-file->html-str content-file)]])
+
+(defn ->archive-entry
+  [file-name]
+  [:div
+   {:class "archive-entry"}
+   [:a {:href file-name} file-name]])
+
+(defn generate-page-with-posts
+  [posts page-file-name post-fn]
+  (io/make-parents page-file-name)
+  (with-open [index-file (io/writer page-file-name :encoding "UTF-8")]
+    (doseq [line (read-template)]
+      (if (string/includes? line "CONTENT")
+        (.write index-file
+                (html [:div
+                       (for [post posts]
+                         (post-fn post))]))
+        (.write index-file line)))
+    (.flush index-file)))
+
 (defn generate-index-page
   "Takes 10 latest posts and creates an HTML page of them."
   [posts site]
   (let [latest          (take 10 (reverse posts))
-        index-file-name (str site "/index.html")
-        format-title    (fn [title]
-                          (as-> (string/split title #".txt") t
-                            (first t)
-                            (LocalDate/parse t date-fmt)
-                            (.format t output-fmt)))]
-    (io/make-parents index-file-name)
-    (with-open [index-file (io/writer index-file-name :encoding "UTF-8")]
-      (doseq [line (read-template)]
-        (if (string/includes? line "CONTENT")
-          (.write index-file
-                  (html[:div
-                        (for [[title content-file] latest]
-                          [:div
-                           [:h3 (format-title title)]
-                           [:div (content-file->html-str content-file)]
-                           [:hr]])]))
-          (.write index-file line)))
-      (.flush index-file))))
+        index-file-name (str site "/index.html")]
+    (generate-page-with-posts latest index-file-name ->post)))
+
+(defn generate-archive-page
+  [posts site]
+  (let [archive-file-name  (str site "/archive.html")
+        posts-by-year     (group-by #(.getYear (key %)) posts)]
+    (doseq [[year posts] posts-by-year]
+      (generate-page-with-posts posts (str site "/" year ".html") ->post))
+    (generate-page-with-posts (map #(str % ".html") (keys posts-by-year))
+                              archive-file-name
+                              ->archive-entry)))
 
 (defn generate-site
   "Generates the website by using the template.html found in the resources
@@ -103,11 +128,10 @@
   [{:keys [thoughts site]}]
   (let [posts             (->> (file-seq (io/file thoughts))
                                (filter #(.isFile %))
-                               (map (fn [f] {(.getName f) f}))
-                               (into (sorted-map)))
-        archive-file-name (str site "/archive.html" )]
-    (io/make-parents archive-file-name)
-    (generate-index-page posts site)))
+                               (map (fn [f] {(thought-title->date(.getName f)) f}))
+                               (into (sorted-map)))]
+    (generate-index-page posts site)
+    (generate-archive-page posts site)))
 
 (defn -main [& args]
   (let [{:keys [message action options]} (parse-args args)]
